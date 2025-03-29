36cb92e86abb01338355d70ae9cfbfaea29f8579
Crystal9123 <crystalcar98@gmail.com>
2025-03-29 23:51:56 +0000

    add story

--- madLibs.js
+++ madLibs.js
@@ -1,4 +1,5 @@
-const story = "The Pirate Adventure";
+const storytitle = "The Pirate Adventure";
+const story = ('One bright morning, I set sail on a pirate ship named The Black Pearl.The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in the water. "Could it be treasure?" the captain exclaimed. We hauled it aboard and opened it. Did you know 5 × 5 equals 25? Incredible, right?');
 const person = "Jimmy";
 const age = 26;
 const timeOfDay = ("Morning");
