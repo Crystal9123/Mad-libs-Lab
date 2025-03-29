b6ffb6abadd9aac6c09d29afba6c757d1023fd86
Crystal9123 <crystalcar98@gmail.com>
2025-03-29 23:36:58 +0000

    added changes to index.html and madLibs.js

--- madLibs.js
+++ madLibs.js
@@ -1,1 +1,40 @@
+const story = "The Pirate Adventure";
+const person = "Jimmy";
+const age = 26;
+const timeOfDay = ("Morning");
+const captain = "Captain Jack";
+
+let treasureChestOpen = true;
+     let answer = true || false;
+    console.log("Logical check",answer);
+
+const num1 = 5;
+const num2 = 5;
+let theProduct = num1*num2
+let op = "multiply";
+if (op === "multiply"){
+    console.log("the product is:", theProduct);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
 
