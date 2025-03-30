dc1598ee11d53dec60fa5b20e23bf1c8057df29a
Crystal9123 <crystalcar98@gmail.com>
2025-03-30 00:03:38 +0000

    added story to console tab

--- madLibs.js
+++ madLibs.js
@@ -1,5 +1,6 @@
 const storytitle = "The Pirate Adventure";
 const story = ('One bright morning, I set sail on a pirate ship named The Black Pearl.The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in the water. "Could it be treasure?" the captain exclaimed. We hauled it aboard and opened it. Did you know 5 × 5 equals 25? Incredible, right?');
+console.log(story);
 const person = "Jimmy";
 const age = 26;
 const timeOfDay = ("Morning");
